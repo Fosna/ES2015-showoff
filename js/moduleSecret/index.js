d4479b90b3273b9ec60c4a7a208bdbfea48d9325
Fosna <vedran.marsic.ka@gmail.com>
2016-04-19 22:15:02 +0200

    Module imports are hoisted.

--- js/moduleSecret/index.js
+++ js/moduleSecret/index.js
@@ -1,9 +1,9 @@
 import * as module from "./module";
+import secretNumber from "./module";
 
 console.log("index.js here!");
 console.log("module.isModule", module.isModule);
 console.log("module.secretNumber", module.secretNumber);
 console.log("module.default", module.default);
-
-import secretNumber from "./module";
 console.log("secretNumber", secretNumber);
+console.log("isModule", isModule);
