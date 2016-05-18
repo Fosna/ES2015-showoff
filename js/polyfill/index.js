f45b087971670a36dc723bf9861b2725249812a1
Fosna <vedran.marsic.ka@gmail.com>
2016-05-18 21:23:44 +0200

    polyfill added

--- js/polyfill/index.js
+++ js/polyfill/index.js
@@ -1,16 +1,22 @@
+import "babel-polyfill";
+
 function timeout(duration = 0) {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, duration);
     })
 }
 
-timeout(1000).then(() => {
+function logSeconds() {
     console.log((new Date()).getSeconds());
+}
+
+timeout(1000).then(() => {
+    logSeconds();
     return timeout(2000);
 }).then(() => {
-    console.log((new Date()).getSeconds());
+    logSeconds();
     
     throw new Error("hmm");
 }).catch(err => {
-    console.log('poking error');
+    console.log("poking error");
 });
